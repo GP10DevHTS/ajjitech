b5058e427f923f064dd108389ce849e4c4b4fc7f
Ahairwe Jordan <jkatetegirwe@gmail.com>
2026-04-23 23:50:55 +0300

    Initial Commit

---
+++ ajjitech/ajjitechApp.swift
@@ -0,0 +1,17 @@
+//
+//  ajjitechApp.swift
+//  ajjitech
+//
+//  Created by Jordan Ahairwe on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ajjitechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
